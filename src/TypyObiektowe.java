de704c6f36dcc1dd42895eac8d9b63af5491596d
WhatCode404 <azaks1nish@gmail.com>
2024-02-29 17:03:32 +0100

    Test 1

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class TypyObiektowe {
+
+    Integer Pierwszy = 1;
+    Integer Drugi = 2;
+
+    public static void main(String[] args) {
+
+        Scanner scan =  new Scanner(System.in);
+        Integer Pierwszy = 1;
+        Integer Drugi = 2;
+        String qwe = "Marcel";
+
+
+
+
+//        System.out.println(Pierwszy);
+//
+//        String Imie = "Marcel";
+//        System.out.println(Imie.charAt(3));
+//
+//        System.out.println("Jak masz na imie?");
+//        String FirstName = scan.nextLine();
+//        System.out.println("Twoje imie to =" + FirstName +"=");
+//
+//        System.out.println("Ile masz lat? ");
+//        Integer first = scan.nextInt();
+//        System.out.println("Twoja liczba lat to: " + first );
+
+
+//
+//        System.out.println("Podaj liczbe kilometrow");
+//        Double kilometry  = scan.nextDouble();
+//        System.out.println("Podaj spalani e auta w litrach: ");
+//        Double litry = scan.nextDouble();
+//        System.out.println("podaj cene paliwa do 2 miejsca po przecinku: ");
+//        Double Kasa = scan.nextDouble();
+//        Double Wynik = litry/100 * kilometry * Kasa;
+//        System.out.println("Koszt przejazdu to: " + Wynik );
+
+//        Integer jeden, dwa, trzy ;
+//
+//        jeden = scan.nextInt();
+//        System.out.println(jeden);
+
+        String text = "Marcel";
+        System.out.println(text);
+
+
+
+    }
+
+
+}
