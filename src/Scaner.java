a034aa9b0812177751d2013c05b71e8f62994911
WhatCode404 <azaks1nish@gmail.com>
2024-03-06 19:02:17 +0100

    Test 1

--- src/Scaner.java
+++ src/Scaner.java
@@ -6,6 +6,9 @@ public class Scaner {
 
         Scanner scan =  new Scanner(System.in);
 
+        System.out.println("Podaj Swoje Imie: ");
+        String imie = scan.nextLine();
+
         System.out.println("Podaj liczbe kilometrow: ");
         Double KM = scan.nextDouble();
         System.out.println("Podaj koszt 1 Litra paliwa: ");
@@ -13,7 +16,7 @@ public class Scaner {
         System.out.println("Podaj spalanie na 100 km: ");
         Double spalanie = scan.nextDouble();
 
-        System.out.println("Koszt przejazdu to: " + KM * koszt_paliwa * spalanie/100 + " zł");
+        System.out.println(imie +", Twoj Koszt przejazdu to: " + KM * koszt_paliwa * spalanie/100 + " zł");
 
     }
 }
